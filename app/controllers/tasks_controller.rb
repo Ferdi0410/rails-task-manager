e9948b82495cf6f04d59d18432e0b91c78b46689
Ferdinand Rieger <Ferdinand.rieger@gmx.de>
2018-05-15 16:12:58 +0200

    finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,41 @@ class TasksController < ApplicationController
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def update
+    @task = Task.find(params[:id])
+    # update the restaurant's attributes
+    @task.update(task_params)
+    # redirect to the restaurant show view
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:title, :details)
   end
 end
